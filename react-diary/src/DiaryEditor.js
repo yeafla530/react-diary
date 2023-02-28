7e94cdd35b4f02847aeea7bf6ff831aa550ab9df
yeafla530 <yeafla530@naver.com>
2023-02-28 17:47:40 +0900

    DOM 조작하기 - useRef

--- react-diary/src/DiaryEditor.js
+++ react-diary/src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 
 const DiaryEditor = () => {
+
+    const authorInput = useRef()
+    const contentInput = useRef()
 
     const [state, setState] = useState({
         author: "",
@@ -18,21 +21,32 @@ const DiaryEditor = () => {
     }
 
     const handleSubmit = () => {
-        console.log(state)
+        // console.log(state)
+        if (state.author.length < 1) {
+            // alert("작성자는 최소 1글자 이상 입력해주세요")
+            // focus
+            authorInput.current.focus();
+            return
+        }
+
+        if (state.contents.length < 5) {
+            // alert("일기 본문은 최소 5글자 이상 입력해주세요")
+            // focus
+            contentInput.current.focus();
+            return
+        }
         alert('저장 성공')
     }
 
-    // const [author, setAuthor] = useState("")
-    // const [contents, setContents] = useState("") 
 
     return <div className="DiaryEditor">
         <h2>일기장</h2>
         {/* 변화가 일어났을 때 setAuthor */}
         <div>
-            <input name="author"type="text" value={state.author} onChange={handleChangeState}/>
+            <input ref={authorInput} name="author"type="text" value={state.author} onChange={handleChangeState}/>
         </div>
         <div>
-            <textarea name="contents" value={state.contents} onChange={handleChangeState}/>
+            <textarea ref={contentInput} name="contents" value={state.contents} onChange={handleChangeState}/>
         </div>
         <div>
             <select name="emotion" value={state.emotion} onChange={handleChangeState}>
