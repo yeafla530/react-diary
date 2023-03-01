b41fceb6813694aa7705dcbb0974e7c58bc6fb57
yeafla530 <yeafla530@naver.com>
2023-03-01 21:43:21 +0900

    useEffect:

--- react-diary/src/Lifecycle.js
+++ react-diary/src/Lifecycle.js
@@ -1,42 +1,33 @@
 import React, {useEffect, useState} from 'react'
 
-const Lifecycle = () => {
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState("")
-
-    // mount (탄생)
+const UnmountTest = () => {
     useEffect(()=>{
-        console.log("mount!");
+        console.log("mount!")
     }, [])
 
-    // update (변화)
     useEffect(()=>{
-        console.log("update!")
-    })
-
-    // count 변화
-    useEffect(()=>{
-        console.log(`count is update: ${count}`)
-        if (count > 5) {
-            alert("count가 5를 넘었습니다 따라서 1로 초기화 됩니다")
-            setCount(1)
+        // unmount시점에 실행됨
+        return () => {
+            console.log("unmount!")
+            
         }
-    },[count])
+    },[])
 
-    // text변화
-    useEffect(()=> {
-        console.log(`text is update: ${text}`)
-    },[text])
-
-    return <div style={{padding: 20}}>
-        <div>
-            {count}
-            <button onClick={() => setCount(count+1)}>+</button>
-        </div>
-        <div>
-            <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
-        </div>
+    return <div>
+        Unmount testing Component
     </div>
+}
+
+const Lifecycle = () => {
+    const [visible, setVisible] = useState(false)
+    const toggle = () => setVisible(!visible)
+    
+    return (
+        <div style={{padding: 20}}>
+            <button onClick={toggle}>On/Off</button>
+            {visible && <UnmountTest />}
+        </div>
+    )
 }
 
 export default Lifecycle
