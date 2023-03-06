33b6d8100f8c30372eaf3e0ecf0be03de3745fce
yeafla530 <yeafla530@naver.com>
2023-03-06 16:53:05 +0900

    react router CSR방식, 페이지 라우팅

---
+++ emotion-diary/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import New from './pages/New'
+import Edit from './pages/Edit'
+import Diary from './pages/Diary'
+import RouteTest from './components/RouteTest'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          {/* url 경로와 component를 mapping시켜줌 */}
+          <Route path='/' element={<Home/>}/>
+          <Route path='/new' element={<New/>}/>
+          <Route path='/edit' element={<Edit/>}/>
+          <Route path='/diary' element={<Diary/>}/>
+
+        </Routes>
+        <RouteTest/>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
