487bb5f7d2af70c3527f6d52ed1330c4053a6bac
yeafla530 <yeafla530@naver.com>
2023-03-19 01:20:26 +0900

    Home 작성중

--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,8 +1,57 @@
+import { useContext, useState, useEffect } from 'react';
+import { DiaryStateContext } from '../App';
+import MyHeader from '../components/MyHeader'
+import MyButton from './../components/MyButton';
 const Home = () => {
+    const diaryList = useContext(DiaryStateContext)
+
+    const [data, setData] = useState([])
+    const [curDate, setCurDate] = useState(new Date())    
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`
+   
+    useEffect(()=>{
+        // 해당 월의 1일이 됨
+        const firstDay = new Date(
+            curDate.getFullYear(),
+            curDate.getMonth(),
+            1
+        ).getTime();
+
+        // 오늘 월의 마지막 날 (30/31/28일)
+        const lastDay = new Date(
+            curDate.getFullYear(),
+            curDate.getMonth()+1,
+            0
+        ).getTime()
+
+        // 바뀐 년월에 따라 리스트 불러오기
+        setData(diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay))
+
+
+
+    },[diaryList, curDate])
+
+    useEffect(()=>{
+        
+    },[data])
+   
+   
+   
+    // 한달씩 미래로
+    const increaseMonth = () => {
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth()+1, curDate.getDate()))
+    }
+    // 한달씩 과거로
+    const decreaseMonth = () => {
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth()-1, curDate.getDate()))
+    }
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>이곳은 Home입니다</p>
+            <MyHeader 
+            headText={headText} 
+            leftChild={<MyButton text={'<'} onClick={decreaseMonth}/>} 
+            rightChild={<MyButton text={'>'} onClick={increaseMonth}/>}/>
         </div>
     )
 }
