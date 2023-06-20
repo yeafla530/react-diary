9b7f597da7ad8d06d5d4f24ec6d94155e19a59de
yeafla530 <yeafla530@naver.com>
2023-06-20 16:16:29 +0900

    [+] 0620 다이어리 리스트 감정 필터 기능 완료

--- emotion-diary/src/components/DiaryList.js
+++ emotion-diary/src/components/DiaryList.js
@@ -5,6 +5,15 @@ const sortOptionList = [
     {value: "oldest", name: "오래된순"},
 
 ]
+
+const selectEmotionList = [
+    {value: "all", name:"모든 감정"},
+    {value: "good", name:"좋은 감정만"},
+    {value: "bad", name:"안좋은 감정만"},
+
+]
+
+
 // 정렬기능
 // onChange : select가 변화했을 때 바꿀 기능을 하는 함수
 // optionList : select tag에 들어가는 옵션
@@ -14,12 +23,24 @@ const ControlMenu = ({value, onChange, optionList})=>{
     </select>
 }
 
+
 const DiaryList = ({diaryList})=> {
 
     const [sortType, setSortType] = useState('latest');
+    const [emotionType, setEmotionType] = useState('all');
+
 
     // DiaryList정렬된 리스트 반환하는 함수
     const getProcessDiaryList = () => {
+        // 감정을 필터링해주는 함수
+        const filterCallBack = (item)=>{
+            if (emotionType === 'good') {
+                return parseInt(item.emotion) <= 3
+            } else {
+                return parseInt(item.emotion) > 3
+            }
+        }
+        // 최신순, 오래된순 정렬해주는 함수
         const compare = (a, b) => {
             // 최신순 정렬
             if (sortType === 'latest') {
@@ -31,15 +52,19 @@ const DiaryList = ({diaryList})=> {
 
         // diaryList를 JSON화 시켜서 문자로 바꾸고 다시 JSON화 시켜줌
         const copyList = JSON.parse(JSON.stringify(diaryList))
-        const sortedList = copyList.sort(compare)
+        const filterredList = emotionType === 'all' ? copyList : copyList.filter((it)=>filterCallBack(it))
+        const sortedList = filterredList.sort(compare)
         return sortedList;
     }
 
 
+
+
     return <div>
         <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
+        <ControlMenu value={emotionType} onChange={setEmotionType} optionList={selectEmotionList}/>
         {getProcessDiaryList().map((it)=>(
-            <div key={it.id}>{it.contents}</div>
+            <div key={it.id}>{it.contents} {it.emotion}</div>
         ))}
     </div>
 }
