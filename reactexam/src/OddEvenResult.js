863c833a3a7b3806d76cf355c4662d8e7d92dab9
yeafla530 <yeafla530@naver.com>
2023-07-11 19:26:29 +0900

    [*] 0710 gitignore 수정

--- reactexam/src/OddEvenResult.js
+++
@@ -1,9 +0,0 @@
-// component자체도 props로 전달 가능
-const OddEvenResult = ({count}) => {
-    // count변화가 없어도 자식요소가 rerender된다
-    // console.log('render')
-    return <>{count % 2 === 0 ? "짝수" : "홀수"}</>
-}
-
-
-export default OddEvenResult
